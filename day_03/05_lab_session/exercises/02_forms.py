ae3b72f5e9ca9fb500177a0429afa7900207b799
Aldwin <aldwinsimon26@gmail.com>
2025-11-16 06:27:30 +0800

    day3_continuation

--- day_03/05_lab_session/exercises/02_forms.py
+++ day_03/05_lab_session/exercises/02_forms.py
@@ -1,4 +1,6 @@
+import json
 import tkinter
+
 
 root = tkinter.Tk()
 name_frame = tkinter.Frame(root)
@@ -13,23 +15,61 @@ entry_name1.pack(side="right")
 age_frame = tkinter.Frame(root)
 age_frame.pack()
 
-age = tkinter.Label(age_frame,text="Age:")
+age = tkinter.Label(age_frame,text="Age:    ")
 entry_age = tkinter.StringVar(age_frame, value="")
 entry_age1 = tkinter.Entry(age_frame, textvariable=entry_age)
 age.pack(side="left")
 entry_age1.pack(side="right")
 
+theme_frame = tkinter.Frame(root)
+theme_frame.pack()
 
-# def save_data(event=None):
-#     data = {
-#         "input data"
-#     }
-# )
-# label.pack()
-#
-# entry_var = tkinter.StringVar(root, value="")
-# entry = tkinter.Entry(root, textvariable=entry_var)
-# entry.pack()
+theme = tkinter.Label(theme_frame, text="Preferred Theme:")
+select_theme1 = tkinter.StringVar(theme_frame, value="Light")
+select_theme2 = tkinter.StringVar(theme_frame, value="Dark")
+choices_theme1 = tkinter.Radiobutton(theme_frame, text="Light", variable=select_theme1)
+choices_theme2 = tkinter.Radiobutton(theme_frame, text="Dark", variable=select_theme2)
+theme.pack(side="left")
+choices_theme1.pack(side="right")
+choices_theme2.pack(side="right")
+
+subscribe_frame = tkinter.Frame(root)
+subscribe_frame.pack()
+
+subscribe = tkinter.BooleanVar()
+# subscribe.set()
+subscribe_checkbox = tkinter.Checkbutton(subscribe_frame, text="Subscribe to newsletter", variable=subscribe)
+subscribe_checkbox.pack(side="right")
+# subscribe_checkbox.select()
+
+
+rate_frame = tkinter.Frame(root)
+rate_frame.pack()
+
+rate_value = tkinter.IntVar(rate_frame, value=0)
+rate = tkinter.Scale(rate_frame, variable=rate_value, from_=0, to=100, orient="horizontal")
+rate.pack(side="right")
+
+class Main:
+    def __init__(self):
+        self.root = tkinter.Tk()
+
+def save_data(forms):
+    data = {
+        "Name:" : entry_name.get(),
+        "Age:" : entry_age1.get(),
+        "Theme:" : select_theme1.get(),
+        "Subscribe:" : subscribe.get(),
+        "Rating:" : rate_value.get(),
+    }
+    forms.append(data)
+    with open('forms.json', 'w') as file:
+        json.dump(forms, file)
+
+def form(event=None):
+    if name = entry_name.get():
+
+    age = entry_age1.get()
 
 # TODO: Create StringVar for name
 # TODO: Create StringVar for name
@@ -48,5 +88,9 @@ entry_age1.pack(side="right")
 
 # TODO: Create function for saving values to JSON
 # TODO: Create button for submit + save
+
+
+# button = tkinter.Button(root,text="Submit",command=check_password)
+# button.pack()
 
 root.mainloop()
